103eca75f854963743a6a8eb2d2262e078a4e415
Jimmy-sama <lungmuss@proton.me>
2023-05-19 10:32:57 +0200

    early stage, NOT ready for production!

--- install.sh
+++ install.sh
@@ -1,9 +1,53 @@
 #!/usr/bin/env bash
+unset CONFIGS
+unset CONFIG_LIST
+unset UPDATE
+unset INSTALL_LIST
+unset FONTS
 
-INSTALL_LIST=("neovim" "pyhton3-neovim" "python3" "pyhton3-pip" "kitty" "ranger")
+CONFIG_LIST=()
+INSTALL_LIST=()
+
+debug "Install everything needed..."
+INSTALL_LIST=("neovim" "pyhton3-neovim" "python3" "pyhton3-pip" "kitty" \
+"ranger" "ninja-build" "cmake" "meson" "gcc-c++" "libxcb-devel" "libX11-devel" \
+"pixman-devel" "wayland-protocols-devel" "cairo-devel" "pango-devel" "gdb" \
+"rust" "cargo" "tldr")
+UPDATE=y
+CONFIGS=y
+FONTS=y
 
 echo "defaultyes=True\nmax_parallel_downloads=6\nfastestmirror=1" >> /etc/dnf/dnf.conf
 
-debug "Updating System"
-debug "$(sudo dnf update -y)"
+for config in $(ls dotconfig); do
+   CONFIG_LIST += ("${config}")
+done
+
+if [[ "${FONTS}" == "y" ]]; then
+   for font in "fontawesome-fonts fontawesome-fonts-web"; do
+      INSTALL_LIST+=("$font")
+   done
+fi
+
+if [[ "${UPDATE}" == "y" ]]; then
+   debug "Updating System..."
+   debug "$(sudo dnf update -y)"
+fi
+
+if [[ "${CONFIGS}" == "y" ]]; then
+   if [[ ! -d ~/.config ]]; then
+      debug "Creating .config directory..."
+      mkdir ~/.config
+   fi
+   debug "copying config files..."
+   for config in ${CONFIG_LIST[@]}; do
+      debug "${config}"
+      cp -r ./dotconfig/${config} ~/.config
+   done
+fi
+
+if [[ ${INSTALL_LIST} ]]; then
+   debug "Installing all Packages..."
+   debug "$(sudo dnf -y install ${INSTALL_LIST[@]})"
+fi
 
